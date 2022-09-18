16c2cda5c95f7de863175c645a9832b3072a4770
Carsten Kemena <c.kemena@uni-muenster.de>
2022-09-18 11:46:43 +0200

    added basic CMake file and isoformCleaner

---
+++ src/isoformCleaner.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file isoformCleaner.cpp
+ * @author Carsten kemena (c.kemena@wwu.de)
+ * @brief File containing the isoformCleanerMain
+ * @version 0.1
+ * @date 2022-09-18
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <cstdlib>
+
+int main (int argc, char *argv[])
+{
+    return EXIT_SUCCESS;
+}
